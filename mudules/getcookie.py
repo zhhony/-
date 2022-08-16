419192c2d70b410e4630639fc5e5a368ee1b93d6
zhhony <zhhony@126.com>
2022-08-16 09:05:40 +0800

    重命名

--- mudules/getcookie.py
+++ mudules/getcookie.py
@@ -1,10 +1,9 @@
 import json
-import time
 from typing import *
 
 
 with open('./resource/config', 'r') as f:
-    BAIDU_BOOL = json.load(f)
+    URL_BOOL = json.load(f)
 
 # cookie文件头，最后一步手工生成cookie会用得到
 COOKIE_HEADER = """# Netscape HTTP Cookie File\n# http://curl.haxx.se/rfc/cookie_spec.html\n# This is a generated file!  Do not edit.\n\n"""
@@ -23,27 +22,27 @@ class Cookie:
         '''将cookie封装成生成器对象，通过next()函数调用'''
         for i in self.cookieList:
             creation_utc, host_key, name, value, path = i
-            cookieContent = str(host_key) + '\t' + str(BAIDU_BOOL[host_key][0] if host_key in BAIDU_BOOL.keys() else 'FALSE') + '\t' + str(path) + '\t' + str(
-                BAIDU_BOOL[host_key][1] if host_key in BAIDU_BOOL.keys() else 'FALSE') + '\t' + str(creation_utc) + '\t' + str(name) + '\t' + str(value)
+            cookieContent = str(host_key) + '\t' + str(URL_BOOL[host_key][0] if host_key in URL_BOOL.keys() else 'FALSE') + '\t' + str(path) + '\t' + str(
+                URL_BOOL[host_key][1] if host_key in URL_BOOL.keys() else 'FALSE') + '\t' + str(creation_utc) + '\t' + str(name) + '\t' + str(value)
             yield cookieContent
 
     def getAll(self):
         '''将cookie所有内容输出到控制台'''
         for i in self.cookieList:
             creation_utc, host_key, name, value, path = i
-            cookieContent = str(host_key) + '\t' + str(BAIDU_BOOL[host_key][0] if host_key in BAIDU_BOOL.keys() else 'FALSE') + '\t' + str(path) + '\t' + str(
-                BAIDU_BOOL[host_key][1] if host_key in BAIDU_BOOL.keys() else 'FALSE') + '\t' + str(creation_utc) + '\t' + str(name) + '\t' + str(value)
+            cookieContent = str(host_key) + '\t' + str(URL_BOOL[host_key][0] if host_key in URL_BOOL.keys() else 'FALSE') + '\t' + str(path) + '\t' + str(
+                URL_BOOL[host_key][1] if host_key in URL_BOOL.keys() else 'FALSE') + '\t' + str(creation_utc) + '\t' + str(name) + '\t' + str(value)
             print(cookieContent)
 
-    def getOutFile(self, path: str = './log/'):
+    def getOutFile(self, filepath: str):
         '''将cookie封装进文件'''
-        with open(path + 'Cookie'+str(int(time.time())) + '.log', 'w') as f:
+        with open(filepath, 'w') as f:
             # 写入头
             f.write(COOKIE_HEADER)
 
             # 写入体
             for i in self.cookieList:
                 creation_utc, host_key, name, value, path = i
-                cookieContent = str(host_key) + '\t' + str(BAIDU_BOOL[host_key][0] if host_key in BAIDU_BOOL.keys() else 'FALSE') + '\t' + str(path) + '\t' + str(
-                    BAIDU_BOOL[host_key][1] if host_key in BAIDU_BOOL.keys() else 'FALSE') + '\t' + str(creation_utc) + '\t' + str(name) + '\t' + str(value) + '\n'
+                cookieContent = str(host_key) + '\t' + str(URL_BOOL[host_key][0] if host_key in URL_BOOL.keys() else 'FALSE') + '\t' + str(path) + '\t' + str(
+                    URL_BOOL[host_key][1] if host_key in URL_BOOL.keys() else 'FALSE') + '\t' + str(creation_utc) + '\t' + str(name) + '\t' + str(value) + '\n'
                 f.write(cookieContent)
