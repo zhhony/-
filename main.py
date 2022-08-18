940b7cde10fcadf7f6d8a7c60e540766fee7b14f
zhhony <zhhony@126.com>
2022-08-18 17:06:16 +0800

    主线程已经可以安全的被阻塞了

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 
 
 cookie.run()  # 执行获取最新cookie的模块
-url.run()  # 执行获取下载链接的模块(注意这一段会启用一般三个子线程，主线程未设置阻塞，所以主线程完成后可能会出现未下载完的情况)
+url.run()  # 执行获取下载链接的模块
 
 
 xlPath = url.getLatestFiles('./data/', 'abc')
